288d60f335b913e3e85dfbfebfd4d0af1e3671b9
German Lashevich <german.lashevich@gmail.com>
2023-04-20 00:11:37 +0200

    Format

--- cmd/sync.go
+++ cmd/sync.go
@@ -6,14 +6,14 @@ import (
 )
 
 func init() {
-    cmd := &cobra.Command{
-        Use:   "sync",
-        Short: "Sync vendir configs",
-        Long:  "Sync vendir configs",
-        Run: func(cmd *cobra.Command, args []string) {
-            log.Info().Msg("Syncing vendir configs")
-        },
-    }
+	cmd := &cobra.Command{
+		Use:   "sync",
+		Short: "Sync vendir configs",
+		Long:  "Sync vendir configs",
+		Run: func(cmd *cobra.Command, args []string) {
+			log.Info().Msg("Syncing vendir configs")
+		},
+	}
 
-    rootCmd.AddCommand(cmd)
+	rootCmd.AddCommand(cmd)
 }
