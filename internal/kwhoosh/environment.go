67d50ed1d2270a4330a946f3f02b6f89f9b1b099
German Lashevich <german.lashevich@gmail.com>
2023-04-23 01:37:17 +0200

    Render environment manifest file

--- internal/kwhoosh/environment.go
+++ internal/kwhoosh/environment.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/rs/zerolog/log"
 	yaml "gopkg.in/yaml.v3"
@@ -16,6 +17,8 @@ type Environment struct {
 	EnvironmentDataFile string
 	// Environment id
 	Id string
+	// Kwhoosh instance
+	k *Kwhoosh
 }
 
 func NewEnvironment(k *Kwhoosh, dir string) *Environment {
@@ -24,14 +27,20 @@ func NewEnvironment(k *Kwhoosh, dir string) *Environment {
 	env := &Environment{
 		Dir:                 dir,
 		EnvironmentDataFile: envDataFile,
+		k:                   k,
 	}
 
-	if err := env.setId(); err == nil {
-		return env
-	} else {
+	if err := env.setId(); err != nil {
 		log.Warn().Err(err).Str("dir", dir).Msg("Unable to set environment id")
 		return nil
 	}
+
+	if err := env.renderManifest(); err != nil {
+		log.Warn().Err(err).Str("dir", dir).Msg("Unable to render environment manifest")
+		return nil
+	}
+
+	return env
 }
 
 func (e *Environment) setId() error {
@@ -62,5 +71,44 @@ func (e *Environment) setId() error {
 
 	e.Id = envData.Environment.Id
 
+	return nil
+}
+
+// Get all environment manifest files up to the root directory
+func (e *Environment) getManifestFiles() []string {
+	currentPath := e.k.RootDir
+	manifestFiles := []string{}
+	for _, level := range strings.Split(e.Dir, filepath.FromSlash("/")) {
+		currentPath = filepath.Join(currentPath, level)
+		manifestFile := filepath.Join(currentPath, e.k.EnvironmentManifestFileName)
+		if _, err := os.Stat(manifestFile); err == nil {
+			manifestFiles = append(manifestFiles, manifestFile)
+		}
+	}
+	log.Debug().Interface("manifestFiles", manifestFiles).Msg("Manifest files")
+	return manifestFiles
+}
+
+// Render the final manifest file for the environment
+func (e *Environment) renderManifest() error {
+	manifestFiles := e.getManifestFiles()
+	if len(manifestFiles) == 0 {
+		return errors.New("No manifest files found")
+	}
+	res, err := YttFiles(manifestFiles)
+	if err != nil {
+		log.Error().Err(err).Str("stderr", res.Stderr).Msg("Unable to render environment manifest")
+		return err
+	}
+	if res.Stdout == "" {
+		return errors.New("Empty output from ytt")
+	}
+	renderedManifestFile := filepath.Join(e.Dir, e.k.RenderedEnvironmentManifestFileName)
+	err = os.WriteFile(renderedManifestFile, []byte(res.Stdout), 0o644)
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to write rendered manifest file")
+		return err
+	}
+	log.Debug().Str("file", renderedManifestFile).Msg("Wrote rendered manifest file")
 	return nil
 }
