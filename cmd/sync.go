cf0fea55a946cdb158f0c1a1a54d066ffbd233f4
German Lashevich <german.lashevich@gmail.com>
2023-04-24 00:28:33 +0200

    Remove prepare command, will use sync instead

--- cmd/sync.go
+++ cmd/sync.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"kwhoosh/internal/kwhoosh"
+
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
@@ -12,6 +14,10 @@ func init() {
 		Long:  "Sync vendir configs",
 		Run: func(cmd *cobra.Command, args []string) {
 			log.Info().Msg("Syncing vendir configs")
+			err := kwhoosh.New(".").Init(nil, nil)
+			if err != nil {
+				log.Fatal().Err(err).Msg("Unable to initialize kwhoosh")
+			}
 		},
 	}
 
