d28544b38df9c40887bc109ed0d8c4fa22e6c442
German Lashevich <german.lashevich@gmail.com>
2023-06-03 02:33:26 +0200

    feat(cli): implement `all` subcommand

--- cmd/all.go
+++ cmd/all.go
@@ -3,15 +3,31 @@ package cmd
 import (
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+
+	"github.com/mykso/myks/internal/myks"
 )
 
 func init() {
 	cmd := &cobra.Command{
 		Use:   "all",
-		Short: "Run prepare, sync, and render",
-		Long:  "Run prepare, sync, and render",
+		Short: "Run sync and render",
+		Long:  "Run sync and render",
 		Run: func(cmd *cobra.Command, args []string) {
-			log.Info().Msg("Running prepare, sync, and render")
+			g := myks.New(".")
+
+			if err := g.Init(targetEnvironments, targetApplications); err != nil {
+				log.Fatal().Err(err).Msg("Unable to initialize myks's globe")
+			}
+
+			log.Info().Msg("Syncing vendir configs")
+			if err := g.Sync(); err != nil {
+				log.Fatal().Err(err).Msg("Unable to sync vendir configs")
+			}
+
+			log.Info().Msg("Rendering manifests")
+			if err := g.Render(); err != nil {
+				log.Fatal().Err(err).Msg("Unable to render applications")
+			}
 		},
 	}
 
