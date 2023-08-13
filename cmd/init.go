46fba3d8b4eeaf10c54a960048b5620f9e4cad88
Kris Budde <kris.budde@gec.io>
2023-08-13 10:34:53 +0200

    feat(init): allow overwriting of data

--- cmd/init.go
+++ cmd/init.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"errors"
+
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 
@@ -14,12 +16,17 @@ func init() {
 		Long:  "Initialize new myks project",
 		Run: func(cmd *cobra.Command, args []string) {
 			g := myks.New(".")
-
-			if err := g.Bootstrap(); err != nil {
+			force, err := cmd.Flags().GetBool("force")
+			if err != nil {
+				log.Fatal().Err(err).Msg("Failed to read flag")
+			}
+			if err := g.Bootstrap(force); errors.Is(err, myks.ErrNotClean) {
+				log.Error().Msg("Directory not empty. Use --force to overwrite data.")
+			} else if err != nil {
 				log.Fatal().Err(err).Msg("Failed to initialize project")
 			}
 		},
 	}
-
+	cmd.Flags().BoolP("force", "f", false, "overwrite existing data")
 	rootCmd.AddCommand(cmd)
 }
