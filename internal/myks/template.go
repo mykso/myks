ca13aad9f8fce0ea0e40567c1fc0b157ae4cce69
German Lashevich <german.lashevich@gmail.com>
2023-08-19 18:47:35 +0200

    refactor: simplify handling of the vendir secret template

--- internal/myks/template.go
+++ internal/myks/template.go
@@ -1,24 +1,26 @@
 package myks
 
 import (
-	"embed"
-	"os"
-	"path/filepath"
+	"bytes"
+	_ "embed"
 
 	"github.com/rs/zerolog/log"
 )
 
-//go:embed all:templates/*.yaml
-var templateFs embed.FS
+//go:embed templates/vendir/secret.ytt.yaml
+var vendirSecretTemplate []byte
 
 func writeSecretFile(secretName string, secretFilePath string, username string, password string) error {
-	err := copyFileSystemToPath(templateFs, "templates", filepath.Join(os.TempDir(), "templates"))
-	if err != nil {
-		return err
-	}
-	res, err := runYttWithFilesAndStdin([]string{filepath.Join(os.TempDir(), "templates", "vendir_secret.ytt.yaml")}, nil, func(name string, args []string) {
-		log.Debug().Msg(msgRunCmd("render vendir secret yaml", name, args))
-	}, "--data-value=secret_name="+secretName, "--data-value=username="+username, "--data-value=password="+password)
+	res, err := runYttWithFilesAndStdin(
+		nil,
+		bytes.NewReader(vendirSecretTemplate),
+		func(name string, args []string) {
+			log.Debug().Msg(msgRunCmd("render vendir secret yaml", name, args))
+		},
+		"--data-value=secret_name="+secretName,
+		"--data-value=username="+username,
+		"--data-value=password="+password,
+	)
 	if err != nil {
 		return err
 	}
