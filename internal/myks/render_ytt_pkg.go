92979b215deca099dae4290f8fe0ac76322c62f1
German Lashevich <german.lashevich@gmail.com>
2023-08-22 01:40:40 +0200

    refactor: small adjustments to ytt-pkg rendering code

--- internal/myks/render_ytt_pkg.go
+++ internal/myks/render_ytt_pkg.go
@@ -24,16 +24,17 @@ func (y *YttPkg) Ident() string {
 func (y *YttPkg) Render(_ string) (string, error) {
 	yttPkgRootDir, err := y.app.getVendoredDir(y.app.e.g.YttPkgStepDirName)
 	if err != nil {
-		log.Err(err).Msg(y.app.Msg(yttPkgStepName, "Unable to get ytt package dir"))
+		log.Error().Err(err).Msg(y.app.Msg(yttPkgStepName, "Unable to get ytt package dir"))
 		return "", err
 	}
+
 	yttPkgSubDirs, err := getSubDirs(yttPkgRootDir)
 	if err != nil {
-		log.Err(err).Msg(y.app.Msg(yttPkgStepName, "Unable to get ytt package sub dirs"))
+		log.Error().Err(err).Msg(y.app.Msg(yttPkgStepName, "Unable to get ytt package sub dirs"))
 		return "", err
 	}
 	if len(yttPkgSubDirs) == 0 {
-		log.Debug().Msg(y.app.Msg(yttPkgStepName, "No ytt package found"))
+		log.Debug().Msg(y.app.Msg(yttPkgStepName, "No ytt packages found"))
 		return "", nil
 	}
 
@@ -41,23 +42,21 @@ func (y *YttPkg) Render(_ string) (string, error) {
 
 	for _, pkgDir := range yttPkgSubDirs {
 		pkgName := filepath.Base(pkgDir)
-		var pkgValuesFile string
-		if pkgValuesFile, err = y.app.prepareValuesFile("ytt-pkg", pkgName); err != nil {
-			log.Warn().Err(err).Msg(y.app.Msg(globalYttStepName, "Unable to prepare vendir packages value files"))
-			return "", err
-		}
 
 		var yttFiles []string
 		for _, yttFile := range y.app.yttPkgDirs {
 			yttFiles = append(yttFiles, filepath.Join(pkgDir, yttFile))
 		}
 		if len(yttFiles) == 0 {
-			yttFiles = append(yttFiles, pkgDir)
+			yttFiles = []string{pkgDir}
 		}
 
 		var yttArgs []string
-		if pkgValuesFile != "" {
-			yttArgs = append(yttArgs, "--data-values-file="+pkgValuesFile)
+		if pkgValuesFile, err := y.app.prepareValuesFile("ytt-pkg", pkgName); err != nil {
+			log.Error().Err(err).Msg(y.app.Msg(yttPkgStepName, "Unable to prepare a values file for the ytt package"))
+			return "", err
+		} else if pkgValuesFile != "" {
+			yttArgs = []string{"--data-values-file=" + pkgValuesFile}
 		}
 
 		res, err := runYttWithFilesAndStdin(yttFiles, nil, func(name string, args []string) {
@@ -65,12 +64,12 @@ func (y *YttPkg) Render(_ string) (string, error) {
 			log.Debug().Msg(msgRunCmd("ytt-pkg render step", name, args))
 		}, yttArgs...)
 		if err != nil {
-			log.Error().Err(err).Str("stdout", res.Stdout).Str("stderr", res.Stderr).Msg(y.app.Msg(globalYttStepName, "Unable to run ytt"))
+			log.Error().Err(err).Str("stdout", res.Stdout).Str("stderr", res.Stderr).Msg(y.app.Msg(yttPkgStepName, "Unable to run ytt"))
 			return "", err
 		}
 
 		if res.Stdout == "" {
-			log.Warn().Str("pkgName", pkgName).Msg(y.app.Msg(globalYttStepName, "No ytt package output"))
+			log.Warn().Str("pkgName", pkgName).Msg(y.app.Msg(yttPkgStepName, "No ytt package output"))
 			continue
 		}
 
