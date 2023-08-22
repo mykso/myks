e342c11cb052303fd9b375a29265a6e37fd9a915
German Lashevich <german.lashevich@gmail.com>
2023-08-23 00:17:16 +0200

    refactor: simplify code of init cmd

--- cmd/init.go
+++ cmd/init.go
@@ -15,18 +15,19 @@ func init() {
 		Short: "Initialize new myks project",
 		Long:  "Initialize new myks project",
 		Run: func(cmd *cobra.Command, args []string) {
-			g := myks.New(".")
 			force, err := cmd.Flags().GetBool("force")
 			if err != nil {
 				log.Fatal().Err(err).Msg("Failed to read flag")
 			}
-			if err := g.Bootstrap(force); errors.Is(err, myks.ErrNotClean) {
+
+			if err := myks.New(".").Bootstrap(force); errors.Is(err, myks.ErrNotClean) {
 				log.Error().Msg("Directory not empty. Use --force to overwrite data.")
 			} else if err != nil {
 				log.Fatal().Err(err).Msg("Failed to initialize project")
 			}
 		},
 	}
+
 	cmd.Flags().BoolP("force", "f", false, "overwrite existing data")
 	rootCmd.AddCommand(cmd)
 }
