07096ef9bdec839b4f8fe4e12e8b71a0c6792bab
German Lashevich <german.lashevich@gmail.com>
2023-09-11 18:18:17 +0200

    fix: apply smart mode logic only to supported commands
    fixes #82

--- cmd/sync.go
+++ cmd/sync.go
@@ -11,11 +11,14 @@ func init() {
 	cmd := &cobra.Command{
 		Use:   "sync",
 		Short: "Sync vendir configs",
-		Long: `Sync vendir configs. This will run vendir sync for all applications. 
+		Long: `Sync vendir configs. This will run vendir sync for all applications.
 
-Authentication against protected repositories is achieved with environment variables prefixed with "VENDIR_SECRET_". 
-For example, if you reference a secret named "mycreds" in your vendir.yaml, you need to export the variables "VENDIR_SECRET_MYCREDS_USERNAME" and 
+Authentication against protected repositories is achieved with environment variables prefixed with "VENDIR_SECRET_".
+For example, if you reference a secret named "mycreds" in your vendir.yaml, you need to export the variables "VENDIR_SECRET_MYCREDS_USERNAME" and
 "VENDIR_SECRET_MYCREDS_PASSWORD" in your environment.`,
+		Annotations: map[string]string{
+			ANNOTATION_SMART_MODE: ANNOTATION_TRUE,
+		},
 		Run: func(cmd *cobra.Command, args []string) {
 			log.Info().Msg("Syncing vendir configs")
 			g := myks.New(".")
