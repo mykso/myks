f2a16c7d37dc4fc2fc39c673421d8ce2b34659c8
German Lashevich <german.lashevich@gmail.com>
2023-11-21 10:25:25 +0000

    chore: debug log rendered applications on disk (#127)

--- internal/myks/environment.go
+++ internal/myks/environment.go
@@ -177,6 +177,7 @@ func (e *Environment) renderedApplications() ([]string, error) {
 			apps = append(apps, dir)
 		}
 	}
+	log.Debug().Strs("apps", apps).Msg(e.Msg("Found rendered applications"))
 	return apps, nil
 }
 
