c5a9596e0a34a08a638d9ed6243f7acb3170171c
German Lashevich <german.lashevich@gmail.com>
2024-04-20 21:45:30 +0200

    fix linter warning and squash code lines

--- cmd/embedded/ytt.go
+++ cmd/embedded/ytt.go
@@ -2,22 +2,15 @@ package embedded
 
 import (
 	"fmt"
-	"math/rand"
 	"os"
-	"time"
 
 	"carvel.dev/ytt/pkg/cmd"
 	uierrs "github.com/cppforlife/go-cli-ui/errors"
 )
 
-// copied from https://github.com/carvel-dev/ytt/blob/develop/cmd/ytt/ytt.go
+// Originated from from https://github.com/carvel-dev/ytt/blob/develop/cmd/ytt/ytt.go
 func yttMain() {
-	rand.Seed(time.Now().UTC().UnixNano())
-
-	command := cmd.NewDefaultYttCmd()
-
-	err := command.Execute()
-	if err != nil {
+	if err := cmd.NewDefaultYttCmd().Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "ytt: Error: %s\n", uierrs.NewMultiLineError(err))
 		os.Exit(1)
 	}
