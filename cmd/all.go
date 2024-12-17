e54e5cca0f1c3e2ae2a32b0dabe2018c78235f9d
German Lashevich <german.lashevich@gmail.com>
2024-12-18 00:17:07 +0100

    overhaul help messages

--- cmd/all.go
+++ cmd/all.go
@@ -8,30 +8,10 @@ import (
 )
 
 var allCmd = &cobra.Command{
-	Use:   "all [environments] [applications]",
-	Short: "Run sync and render for selected environments and applications",
-	Long: `Run sync and render for selected environments and applications
-
-Usage:
-  myks all [environments] [applications] [flags]
-
-Environments:
-  A comma-separated list of environments to render. If you provide "ALL", all environments will be rendered.
-
-Applications:
-  A comma-separated list of applications to render. If you don't provide this argument or provide "ALL", all applications will be rendered.
-
-Flags:
-  -h, --help   help for all
-
-Global Flags:
-  -a, --async int                         sets the number of applications to be processed in parallel
-                                          the default (0) is no limit
-      --config string                     config file (default is the first .myks.yaml up the directory tree)
-  -l, --log-level string                  set the logging level (default "info")
-      --smart-mode.base-revision string   base revision to compare against in Smart Mode
-                                          if not provided, only local changes will be considered
-      --smart-mode.only-print             only print the list of environments and applications that would be rendered in Smart Mode`,
+	Use:   "all",
+	Short: "Run sync and render",
+	Long:  "Run sync and render for specified environments and applications",
+	Args:  cobra.RangeArgs(0, 2),
 	Annotations: map[string]string{
 		ANNOTATION_SMART_MODE: ANNOTATION_TRUE,
 	},
