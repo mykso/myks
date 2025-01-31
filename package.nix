76331fb92b2a64649a3fe1b3171ec684c0e0f3a4
German Lashevich <german.lashevich@gmail.com>
2025-01-31 13:59:11 +0100

    chore(release): trying to simplify the setup

--- package.nix
+++ package.nix
@@ -2,7 +2,7 @@
   pkgs,
   self,
 }: let
-  baseVersion = "4.2.3"; # x-release-please-version
+  baseVersion = builtins.fromJSON (builtins.readFile ./.version.json).".";
   commit = self.shortRev or self.dirtyShortRev or "unknown";
   version = "${baseVersion}-${commit}";
 in
