9a083c305b3019914dfc9dff4de681f3efbbf8e4
German Lashevich <german.lashevich@gmail.com>
2025-02-06 14:43:58 +0100

    dump

--- internal/myks/plugin_static_files.go
+++ internal/myks/plugin_static_files.go
@@ -9,38 +9,17 @@ import (
 const StaticFilesStepName = "static-files"
 
 func (a *Application) copyStaticFiles() error {
-	logStaticFiles := func(files []string) {
-		log.Trace().Strs("staticFiles", files).Msg(a.Msg(StaticFilesStepName, "Static files"))
-	}
-	staticFilesDirs := []string{}
+	if staticFilesDirs, err := a.collectTreeFiles(a.e.g.StaticFilesDirName); err == nil {
+		staticFilesDestination := filepath.Join(a.getDestinationDir(), a.e.g.StaticFilesDirName)
 
-	// 1. Static files from the prototype
-	prototypeStaticFilesDir := filepath.Join(a.Prototype, a.e.g.StaticFilesDirName)
-	if ok, err := isExist(prototypeStaticFilesDir); err != nil {
-		return err
-	} else if ok {
-		staticFilesDirs = append(staticFilesDirs, prototypeStaticFilesDir)
-	}
-	logStaticFiles(staticFilesDirs)
-	// 2. Static files from prototype overrides
-	staticFilesDirs = append(staticFilesDirs, a.e.collectBySubpath(filepath.Join(a.e.g.PrototypeOverrideDir, a.prototypeDirName(), a.e.g.StaticFilesDirName))...)
-	logStaticFiles(staticFilesDirs)
-
-	// 3. Static files from the environment
-	staticFilesDirs = append(staticFilesDirs, a.e.collectBySubpath(filepath.Join(a.e.g.EnvsDir, a.e.g.StaticFilesDirName))...)
-	logStaticFiles(staticFilesDirs)
-	// 4. Static files from the application
-	staticFilesDirs = append(staticFilesDirs, a.e.collectBySubpath(filepath.Join(a.e.g.AppsDir, a.Name, a.e.g.StaticFilesDirName))...)
-	logStaticFiles(staticFilesDirs)
-
-	staticFilesDestination := filepath.Join(a.getDestinationDir(), a.e.g.StaticFilesDirName)
-
-	for _, staticFilesDir := range staticFilesDirs {
-		if err := copyDir(staticFilesDir, staticFilesDestination, true); err != nil {
-			log.Error().Err(err).Msg(a.Msg(StaticFilesStepName, "Unable to copy static files"))
-			return err
+		for _, staticFilesDir := range staticFilesDirs {
+			if err = copyDir(staticFilesDir, staticFilesDestination, true); err != nil {
+				log.Error().Err(err).Msg(a.Msg(StaticFilesStepName, "Unable to copy static files"))
+				return err
+			}
 		}
+		return nil
+	} else {
+		return err
 	}
-
-	return nil
 }
