1b514033f49bd97a120accca098eaedd7e72b3de
German Lashevich <german.lashevich@gmail.com>
2025-10-20 18:09:17 +0200

    .

--- cmd/embedded/kbld.go
+++ cmd/embedded/kbld.go
@@ -3,19 +3,14 @@ package embedded
 import (
 	"io"
 	"log"
-	"math/rand"
 	"os"
-	"time"
 
 	carvelCmd "carvel.dev/kbld/pkg/kbld/cmd"
 	uierrs "github.com/cppforlife/go-cli-ui/errors"
 	"github.com/cppforlife/go-cli-ui/ui"
 )
 
-// copied from https://github.com/carvel-dev/kbld/blob/develop/cmd/kbld/kbld.go
 func kbldMain() {
-	rand.New(rand.NewSource(time.Now().UTC().UnixNano())) // #nosec G404 -- should be fixed upstream
-
 	log.SetOutput(io.Discard)
 
 	// TODO logs
@@ -31,6 +26,4 @@ func kbldMain() {
 		confUI.ErrorLinef("kbld: Error: %v", uierrs.NewMultiLineError(err))
 		os.Exit(1)
 	}
-
-	confUI.PrintLinef("Succeeded")
 }
